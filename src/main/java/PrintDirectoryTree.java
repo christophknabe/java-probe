0ed1d8c9b9d09d93e348dec6a91c63af56a90e9b
Christoph Knabe <knabe@beuth-hochschule.de>
2022-09-15 15:26:12 +0200

    Recursive printDirectoryTree by java.io.File.

--- src/main/java/PrintDirectoryTree.java
+++ src/main/java/PrintDirectoryTree.java
@@ -1,14 +1,29 @@
+import java.io.File;
+
+/** Prints the tree of the current working directory. Similar to <code>ls -R</code>
+ * @author Christoph Knabe
+ * @since 2022-09-15
+ */
 public class PrintDirectoryTree {
+
+    public static void main(final String... args){
+        new PrintDirectoryTree().execute();
+    }
+
+    private void execute(){
+        printDirectoryTree(new File(".").getAbsoluteFile(), 0);
+    }
   
     private void printDirectoryTree(final File file, final int level){
         final String printName = level<=0 ? file.getAbsolutePath() : file.getName();
         printIndentation(level);
         System.out.println(printName);
-        if(file.isDirectory()){
-            final File[] subfiles = file.listFiles();
-            for(final File subfile: subfiles){
-                printDirectoryTree(subfile, level+1);
-            }
+        final File[] subfiles = file.listFiles();
+        if(subfiles==null){ //no directory => break recursion here
+            return;
+        }
+        for(final File subfile: subfiles){
+            printDirectoryTree(subfile, level+1);
         }
     }
 
